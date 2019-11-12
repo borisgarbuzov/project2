6355ca7338184842a466ba51dd1a3b2377d89db0
Maxim Makovskiy <makovskiyms@gmail.com>
2019-11-12 09:53:14 +0000

    finish code for first use case plot

--- lab/Ma/first_use_case.py
+++ lab/Ma/first_use_case.py
@@ -8,11 +8,11 @@ from first_use_case_plot import *
 def first_use_case(sample_size_min, sample_size_max, sample_size_by,
                    mean, sigma):
     # create so called par_list for subtitle in plot
-    par_list = {sample_size_min: sample_size_min,
-                sample_size_max: sample_size_max,
-                sample_size_by: sample_size_by,
-                mean: mean,
-                sigma: sigma}
+    par_list = {"sample_size_min": sample_size_min,
+                "sample_size_max": sample_size_max,
+                "sample_size_by": sample_size_by,
+                "mean": mean,
+                "sigma": sigma}
 
     # here we create so called sample_size_array that contains all sample sizes
     sample_size_array = np.arange(start = sample_size_min,
@@ -33,7 +33,10 @@ def first_use_case(sample_size_min, sample_size_max, sample_size_by,
             sample = sample)
         threshold_array[sample_size_index] = threshold_estimator(
             sample = sample)
+        print("Sample sizes left", sample_size_array.size -
+              (sample_size_index + 1))
 
+    # plot two estimates against sample sizes
     first_use_case_plot(sample_size_array = sample_size_array,
                         newey_west_array = newey_west_array,
                         threshold_array = threshold_array,
--- lab/Ma/first_use_case_plot.py
+++ lab/Ma/first_use_case_plot.py
@@ -1,10 +1,39 @@
+import os
+import re
 import matplotlib.pyplot as plt
 
 
 def first_use_case_plot(sample_size_array, newey_west_array,
                         threshold_array, par_list):
+
+    # create directory for output if doesn't exist
+    if not os.path.exists('output'):
+        os.makedirs('output')
+
+    # create file name for plot
+    file_name = 'output/plot case one ' + str(par_list).replace(': ', '=') + '.png'
+
+    # prepare par_list for subtitle
+    symbols = re.compile(r'[{\'\'}]', flags = re.UNICODE)
+    caption_text = symbols.sub("", str(par_list)).replace(':', ' =')
+
+    plt.style.use('seaborn')
+
     plt.plot(sample_size_array, newey_west_array, color = 'blue',
              label = 'Newey-West')
-    plt.plot(sample_size_array, threshold_array, color = "red",
-             label = "Threshold")
-    plt.legend(framealpha = 1, frameon = True)
+    plt.plot(sample_size_array, threshold_array, color = 'red',
+             label = 'Threshold')
+
+    plt.xlabel('sample size')
+    plt.ylabel('value')
+    plt.title('Plot for first use case')
+
+    # legend automatically choose best place
+    plt.legend(framealpha = 1, frameon = False)
+
+    plt.tight_layout()
+
+    # add subtitle on our plot
+    plt.text(x = 5000, y = -1, s = caption_text, ha='center', va='center')
+
+    plt.savefig(file_name, dpi = 300, bbox_inches = 'tight')
