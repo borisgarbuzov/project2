18f53ecf5c43b5663ea7e4e14a9c19297494ba0b
Nikita-lys <lysenkonikita61@yandex.ru>
2019-11-18 16:50:31 +0300

    add estimate_nw

--- lab/N/third_use_case.py
+++ lab/N/third_use_case.py
@@ -29,10 +29,10 @@ class MA1(TimeSeries):
     def __init__(self, sample_size, coef=1):
         self.sample_size = sample_size
         self.coef = coef
-        self.sample = np.zeros(self.sample_size)
         self.generate()
 
     def generate(self):
+        self.sample = np.zeros(self.sample_size)
         noise = np.random.normal(size=self.sample_size + 1)
         for i in range(self.sample_size):
             self.sample[i] = noise[i + 1] + self.coef * noise[i]
