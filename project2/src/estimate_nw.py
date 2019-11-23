add precision and fix estimate_nw

project2/src/estimate_nw.py
@@ -1,23 +1,26 @@
 from src.diagonal_sample_tvma1 import diagonal_sample_tvma1
 from src.cov_matrix import cov_matrix
-from src.custom_kernel import triangular_kernel
-from src.b_nw import b_nw
+from src.create_t_par_array import create_t_par_array
+from src.true_lrv import true_lrv_ma1
+import src.custom_kernel
+import src.precision
 import numpy as np
 
 
 def estimate_nw(cov_matrix: np.array) -> np.array:
     """
-    estimate Newey-West.
+    estimate newey west.
 
     :param cov_matrix: covariance double array
-    :return: array of Newey-West
+    :return: array of newey west
     """
     t_par_count = len(cov_matrix[0])
     res_array = np.full(shape=t_par_count, fill_value=0.0)
 
     for lag in range(len(cov_matrix)):
-        b_nw_value = b_nw(sample_size=t_par_count)
-        K = triangular_kernel(lag / (t_par_count * b_nw_value))
+        # b_nw = compute_b_nw(t_par_count=t_par_count)
+        # K = custom_kernel(u=lag, bandwidth=b_nw, kernel_type='triangular')
+        K = custom_kernel.triangular_kernel(v=lag)
         for t_par_index in range(t_par_count):
             res_array[t_par_index] += cov_matrix[lag][t_par_index] * K
 
@@ -25,11 +28,24 @@ def estimate_nw(cov_matrix: np.array) -> np.array:
 
 
 if __name__ == '__main__':
+    t_par_count = 11
+    sigma = 2
+
     diagonal_sample = diagonal_sample_tvma1(sample_size=20,
                                             mean=0,
-                                            sigma=2,
+                                            sigma=sigma,
                                             noise_type='bernoulli')
 
-    cov_double_array = cov_matrix(sample=diagonal_sample, t_par_count=11)
+    cov_double_array = cov_matrix(sample=diagonal_sample,
+                                         t_par_count=t_par_count)
 
-    print(estimate_nw(cov_matrix=cov_double_array))
+    # newey west array
+    nw_array = estimate_nw(cov_matrix=cov_double_array)
+
+    true_array = np.full(shape=t_par_count, fill_value=np.nan)
+    t_par_array = create_t_par_array(t_par_count)
+    for index, t_par in enumerate(t_par_array):
+        true_array[index] = true_lrv_ma1(sigma=sigma, t_par=t_par)
+
+    mse_precision = precision.mse(true_array=true_array, hat_double_array=nw_array)
+    print(mse_precision)

project2/src/precision.py
@@ -1,36 +1,41 @@
 import numpy as np
 
 
-def compute_precision(true_array, hat_double_array):
+def mse(true_array: np.array, hat_double_array: np.array) -> np.array:
     """
-    compute Mean, Bias, Variance, MSE
+    Mean squared error
 
-    :param true_array:
-    :param hat_double_array:
-    :return:list of precisions
+    :param true_array: array of true values
+    :param hat_double_array: array
+    :return:
     """
     t_par_count = len(true_array)
-    mean_array = list()
-    bias_array = list()
-    variance_array = list()
-    mse_array = list()
-
+    mse_array = np.full(shape=t_par_count, fill_value=np.nan)
     for t in range(t_par_count):
-        mean_array.append(np.mean(hat_double_array[t]))
-        variance_array.append(np.var(hat_double_array[t]))
-        bias_array.append(mean_array[t] - true_array[t])
-        mse_array.append(np.mean((hat_double_array[t] - true_array[t])**2))
-
-    return [mean_array, bias_array, variance_array, mse_array]
+        mse_array[t] = (np.mean((hat_double_array[t] - true_array[t]) ** 2))
+    return mse_array
 
 
-def mse(true_array, hat_double_array):
-    value = 0
-    return value
-    
-    
-    
-    
-    
-    
-    
+def mean(true_array: np.array, hat_double_array: np.array) -> np.array:
+    t_par_count = len(true_array)
+    mean_array = np.full(shape=t_par_count, fill_value=np.nan)
+    for t in range(t_par_count):
+        mean_array[t] = (np.mean(hat_double_array[t]))
+    return mean_array
+
+
+def bias(true_array: np.array, hat_double_array: np.array) -> np.array:
+    t_par_count = len(true_array)
+    bias_array = np.full(shape=t_par_count, fill_value=np.nan)
+    mean_array = mean(true_array=true_array, hat_double_array=hat_double_array)
+    for t in range(t_par_count):
+        bias_array[t] = mean_array[t] - true_array[t]
+    return bias_array
+
+
+def variance(true_array: np.array, hat_double_array: np.array) -> np.array:
+    t_par_count = len(true_array)
+    variance_array = np.full(shape=t_par_count, fill_value=np.nan)
+    for t in range(t_par_count):
+        variance_array[t] = np.var(hat_double_array[t])
+    return variance_array