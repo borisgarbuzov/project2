87d8893c6e8045d9fe4aba8f1110c13a123c28b0
Makovskiy Maxim <makovskiyms@gmail.com>
2019-12-03 19:21:42 +0600

    test for lrv_hat_of_t_nw_2

---
+++ project2/test/test_lrv_hat_of_t_nw_2.py
@@ -0,0 +1,35 @@
+from src.lrv_hat_of_t_nw_2 import lrv_hat_of_t_nw_2
+from timeit import default_timer as timer
+import numpy as np
+import unittest
+
+
+class Test_lrv_hat_of_t_nw_2(unittest.TestCase):
+    def test_lrv_hat_of_t_nw_2(self, sample=np.array([1, 2, 3, 4, 5]),
+                               t_par=0.5):
+
+        print('\n\n===============================================================================')
+        print('Testing "lrv_hat_of_t_nw_2"')
+
+        start_time = timer()
+
+        returned = lrv_hat_of_t_nw_2(sample=sample,
+                                     t_par=t_par)
+        print('Test parameters:')
+        print('sample =', sample)
+        print('t_par =', t_par)
+
+        print('\nreturned = ', type(returned))
+        if isinstance(returned, list):
+            print('returned shape = ', len(returned))
+        elif isinstance(returned, np.ndarray):
+            print('returned shape = ', returned.shape)
+        print('returned = ', returned)
+
+        print("\nDuration: {:g} secs".format(timer() - start_time))
+        print('End of test {}'.format('lrv_hat_of_t_nw_2'))
+        print('===============================================================================\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
