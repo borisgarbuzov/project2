8c73b3d49f0f0f85645d0f05be24f9fc93b7557b
Elisejj <elisricov@yandex.ru>
2019-12-08 17:52:37 +0000

    tHreshold

---
+++ project2/src/threshold_indicator.py
@@ -0,0 +1,10 @@
+import numpy as np
+from src.sd_cov_hat import sd_cov_hat
+from src.zhou_treshold import zhou_treshold
+
+def threshold_indicator(sample_size, cov_hat, lag):
+    indicator_value = 0
+    ratio = abs(cov_hat) / sd_cov_hat(sample_size)
+    is_ratio_smaller = (ratio < zhou_treshold(sample_size))
+    indicator_value = int(is_ratio_smaller)
+    return indicator_value
