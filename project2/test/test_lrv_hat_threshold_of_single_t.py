31d50233c3cf32755d55ba6eae09641b70c94740
Elisejj <elisricov@yandex.ru>
2019-12-11 14:23:08 +0000

    test lrv

---
+++ project2/test/test_lrv_hat_threshold_of_single_t.py
@@ -0,0 +1,28 @@
+from src.lrv_hat_threshold_of_single_t import lrv_hat_threshold_of_single_t
+from timeit import default_timer as timer
+import numpy as np
+import unittest
+
+class Test_lrv_hat_threshold_of_single_t(unittest.TestCase):
+    def test_lrv_hat_threshold_of_single_t(self,
+                                            cov_hat_column = np.array([1,  1000]),
+                                            sample_size = 10):
+        print('\n\n===============================================================================')
+        print('Testing "lrv_hat_threshold_of_single_t"')
+
+        start_time = timer()
+        returned = lrv_hat_threshold_of_single_t(cov_hat_column = cov_hat_column, sample_size = sample_size)
+        duration = timer() - start_time
+        expected = 1
+        print('Test parameters:')
+        print('cov_hat_column = ', cov_hat_column)
+        print('sample_size = ', sample_size)
+        print('expected = ', expected)
+        print('returned = ', returned)
+        print("\nDuration: {:g} secs".format(timer() - start_time))
+        print('End of test {}'.format('lrv_hat_threshold_of_single_t'))
+        print('===============================================================================\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
