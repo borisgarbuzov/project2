51795a58de5351044874e490712bf5a402673f0c
Ubuntu <ubuntu@ip-172-31-33-40.us-east-2.compute.internal>
2019-12-15 13:08:00 +0000

    comments

--- project2/src/threshold_max_lag.py
+++ project2/src/threshold_max_lag.py
@@ -1,3 +1,10 @@
+# Threshold max lag. 
+# Initially Zhou said, n/2 or sqrt(n).
+# then degree 1/3?
+# No restriction is imposed from the NW side. 
+# The only concern for us is performance. 
+# And performance we can measure only by multiple experiments
+# recording the precision. 
 def threshold_max_lag(sample_size: int) -> float:
 	const = 1
 	degree = 1/3
