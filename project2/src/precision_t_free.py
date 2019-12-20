4cb0030fb4d49ddd6fce3cc62c6686613557626d
Elisejj <elisricov@yandex.ru>
2019-12-20 05:48:33 +0000

    precisions

--- project2/src/precision_t_free.py
+++ project2/src/precision_t_free.py
@@ -1,17 +1,18 @@
 import numpy as np
 from sklearn.metrics import mean_squared_error
-from src.bias_t_free import bias_t_free
+import src.bias_t_free
 
 def mean_t_free(est_array: np.array):
     return np.mean(est_array)
 
 def bias_t_free(true_value: float, est_array: np.array):
-    return bias_t_free(est_array, true_value)
+    mean = np.mean(est_array)
+    return mean - true_value
 
 def variance_t_free(est_array: np.array):
     return np.var(est_array)
     
-def mse_t_free(true_value: float, est_array: np.array):
+def mse_t_free(true_array: np.array, est_array: np.array):
     return mean_squared_error(true_array, est_array)
 
 def precision_t_free(true_value: float, est_array: np.array):
---
+++ project2/test/test_precision_t_free.py
@@ -0,0 +1,74 @@
+import unittest
+import numpy as np
+from timeit import default_timer as timer
+import src.precision_t_free
+
+class Test_run_precision_t_free(unittest.TestCase):
+    def test_run_mean_t_free(self, est_array=[1,2,3]):
+        print('\n\n===============================================================================')
+        print('RUN testing "mean_t_free"')
+        start_time = timer()
+        returned = src.precision_t_free.mean_t_free(est_array)
+        duration = timer() - start_time
+        print('Test parameters:')
+        print('est_array = ', est_array)
+        
+        print('\nreturned = ', type(returned))
+        print('returned = ', returned)
+            
+        print("\nDuration: {:g} secs".format(duration))
+        print('End of RUN test {}'.format('mean_t_free'))
+        print('===============================================================================\n')
+    
+    def test_run_variance_t_free(self, est_array=[1,2,3]):
+        print('\n\n===============================================================================')
+        print('RUN testing "variance_t_free"')
+        start_time = timer()
+        returned = src.precision_t_free.variance_t_free(est_array)
+        duration = timer() - start_time
+        print('Test parameters:')
+        print('est_array = ', est_array)
+
+        print('\nreturned = ', type(returned))
+        print('returned = ', returned)
+            
+        print("\nDuration: {:g} secs".format(duration))
+        print('End of RUN test {}'.format('variance_t_free'))
+        print('===============================================================================\n')
+        
+    def test_run_mse_t_free(self, est_array=[1,2,3], true_array=[3,4,5]):
+        print('\n\n===============================================================================')
+        print('RUN testing "mse_t_free"')
+        start_time = timer()
+        returned = src.precision_t_free.mse_t_free(true_array, est_array)
+        duration = timer() - start_time
+        print('Test parameters:')
+        print('est_array = ', est_array)
+        print('true_value = ', true_value)
+
+        print('\nreturned = ', type(returned))
+        print('returned = ', returned)
+            
+        print("\nDuration: {:g} secs".format(duration))
+        print('End of RUN test {}'.format('mse_t_free'))
+        print('===============================================================================\n')
+
+    def test_bias_t_free(self, est_array=[1,2,3], true_value=5):
+        print('\n\n===============================================================================')
+        print('RUN testing "bias_t_free"')
+        start_time = timer()
+        returned = src.precision_t_free.bias_t_free(true_value, est_array)
+        duration = timer() - start_time
+        print('Test parameters:')
+        print('est_array = ', est_array)
+        print('true_value = ', true_value)
+
+        print('\nreturned = ', type(returned))
+        print('returned = ', returned)
+            
+        print("\nDuration: {:g} secs".format(duration))
+        print('End of RUN test {}'.format('bias_t_free'))
+        print('===============================================================================\n')
+    
+if __name__ == '__main__':
+    unittest.main()
