a98f6cd55f232ac488a88c99a5a7f7cd30b3351c
Elisejj <elisricov@yandex.ru>
2019-12-20 05:35:46 +0000

    precision

--- project2/src/precision_t_free.py
+++ project2/src/precision_t_free.py
@@ -8,7 +8,7 @@ def mean_t_free(est_array: np.array):
 def bias_t_free(true_value: float, est_array: np.array):
     return bias_t_free(est_array, true_value)
 
-def variance_r_free(est_array: np.array):
+def variance_t_free(est_array: np.array):
     return np.var(est_array)
     
 def mse_t_free(true_value: float, est_array: np.array):
@@ -17,5 +17,5 @@ def mse_t_free(true_value: float, est_array: np.array):
 def precision_t_free(true_value: float, est_array: np.array):
     return_of_mean_t_free = mean_t_free(est_array)
     return_of_bias_t_free = bias_t_free(true_value, est_array)
-    return_of_variance_r_free = variance_r_free(est_array)
+    return_of_variance_t_free = variance_t_free(est_array)
     return_of_mse_t_free = mse_t_free(true_value, est_array)
