bf5f05e7d3fa81444c9ced39a6b28c358749a20d
Elisejj <elisricov@yandex.ru>
2019-12-25 15:22:29 +0000

    N

--- project2/src/lrv_hat_threshold_t_free.py
+++ project2/src/lrv_hat_threshold_t_free.py
@@ -5,13 +5,22 @@ import numpy as np
 def lrv_hat_threshold_t_free(cov_hat_column: np.array,
                              sample_size: int) -> int:
     max_lag = len(cov_hat_column)
-    cum_sum = cov_hat_column[0] * threshold_indicator(sample_size=sample_size,
+    indicator_array = []
+    print('--------------------------------------------------')
+    print('cov_hat_column=', cov_hat_column)
+    print('sample_size=', sample_size)
+    print('max_lag=', max_lag)
+    indicator_value = threshold_indicator(sample_size=sample_size,
                                                      cov_hat=cov_hat_column[0],
                                                      lag=0)
-
+    indicator_array.append(indicator_value)
+    cum_sum = cov_hat_column[0] * indicator_value
     for lag in range(1, max_lag):
-        term = 2 * cov_hat_column[lag] * threshold_indicator(
+        indicator_value = threshold_indicator(
             sample_size=sample_size, cov_hat=cov_hat_column[lag], lag=lag)
+        term = 2 * cov_hat_column[lag] * indicator_value
+        indicator_array.append(indicator_value)
         cum_sum += term
-
+    print('indicator_array=', indicator_array)
+    print('--------------------------------------------------')
     return cum_sum
