c9b5a6cee8e4374f4af9a1a08920254687b3b7ac
Elisejj <elisricov@yandex.ru>
2019-12-30 07:53:44 +0000

    test compute and save

--- project2/src/compute_and_save_var_cov_hat_theoretical_matrix.py
+++ project2/src/compute_and_save_var_cov_hat_theoretical_matrix.py
@@ -7,7 +7,7 @@ import os
 
 
 def compute_and_save_var_cov_hat_theoretical_matrix(sample_size_array: np.array, lags_array: np.array, noise_type: str,
-                                                    is_data=False) -> np.arra:
+                                                    is_data=False) -> np.array:
     """
     Compute and save into csv theoretical var cov hat matrixes with corrects noise_type and lags.
 
