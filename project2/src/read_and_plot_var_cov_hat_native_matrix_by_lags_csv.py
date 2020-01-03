261f030719b4f2791512c182a9594f58965c4734
Nikita-lys <lysenkonikita61@yandex.ru>
2020-01-03 16:56:42 +0300

    add plot for var_cov_hat_native_matrix by sample_size and by lags

---
+++ project2/src/read_and_plot_var_cov_hat_native_matrix_by_lags_csv.py
@@ -0,0 +1,52 @@
+from os.path import dirname
+import numpy as np
+import pandas as pd
+import os
+import re
+from src.plot_arrays import plot_arrays
+
+
+def read_and_plot_var_cov_hat_native_matrix_by_lags_csv(noise_type: str, fix_number_of_lags=None) -> None:
+    """
+    Plot var cov hat vs lags
+    Read from csv and plot len(sample_size_array) var cov hat lines:
+
+    :param noise_type: type of noise 'gaussian' or 'bernoulli'
+    :param fix_number_of_lags: for take cur csv
+    :return: plot len(sample_size_array) lines
+    """
+    parent_dir = dirname(dirname(__file__))
+    data_folder = os.path.join(parent_dir, "data")
+
+    # read csv
+    name = ''
+    if fix_number_of_lags:
+        name = '_{}_lags'.format(fix_number_of_lags)
+    native_matrix_csv = 'var_cov_hat_native_matrix_{}{}.csv'.format(noise_type, name)
+    native_matrix = pd.read_csv(
+        os.path.join(data_folder, native_matrix_csv),
+        index_col='lag')
+
+    lags_array = [int(re.sub("[^0-9]", "", lag)) for
+                         lag in native_matrix.index]
+
+    sample_size_array = [int(re.sub("[^0-9]", "", sample_size)) for
+                         sample_size in native_matrix.columns]
+
+    arrays_dict = dict()
+    for sample_size in sample_size_array:
+
+        # get column
+        array_for_plot = native_matrix['sample size {}'.format(sample_size)]
+
+        # create dict for plotting
+        arrays_dict['sample size {}'.format(sample_size)] = array_for_plot
+
+    plot_arrays(x_array=lags_array,
+                arrays_dict=arrays_dict,
+                title="native matrix with {} noise vs lags".format(noise_type),
+                x_label="sample size")
+
+
+if __name__ == '__main__':
+    read_and_plot_var_cov_hat_native_matrix_by_lags_csv(noise_type='gaussian', fix_number_of_lags=300)
---
+++ project2/src/read_and_plot_var_cov_hat_native_matrix_by_sample_size_csv.py
@@ -0,0 +1,54 @@
+from os.path import dirname
+import numpy as np
+import pandas as pd
+import os
+import re
+from src.plot_arrays import plot_arrays
+
+
+def read_and_plot_var_cov_hat_native_matrix_by_sample_size_csv(noise_type: str, count_lags: np.array, fix_number_of_lags=None) -> None:
+    """
+    Plot var cov hat vs sample size
+    Read from csv and plot len(count_lags) var cov hat lines:
+
+    :param noise_type: type of noise 'gaussian' or 'bernoulli'
+    :param count_lags: array of lags, example: [0,1,2,3,4,5]
+    :param fix_number_of_lags: for take cur csv
+    :return: plot len(count_lags) lines
+    """
+    parent_dir = dirname(dirname(__file__))
+    data_folder = os.path.join(parent_dir, "data")
+
+    # read csv
+    name = ''
+    if fix_number_of_lags:
+        name = '_{}_lags'.format(fix_number_of_lags)
+    native_matrix_csv = 'var_cov_hat_native_matrix_{}{}.csv'.format(noise_type, name)
+    native_matrix = pd.read_csv(
+        os.path.join(data_folder, native_matrix_csv),
+        index_col='lag')
+
+    sample_size_array = [int(re.sub("[^0-9]", "", sample_size)) for
+                         sample_size in native_matrix.columns]
+
+    matrix_for_plot = np.full(shape=(len(count_lags), len(sample_size_array)), fill_value=np.NaN)
+
+    # get 1 row from csv
+    for lag in count_lags:
+        row = native_matrix[lag:lag+1]
+        matrix_for_plot[lag] = row
+
+    # create dict for plotting
+    arrays_dict = dict()
+    for lag in count_lags:
+        arrays_dict['lag {}'.format(lag)] = matrix_for_plot[lag]
+
+    plot_arrays(x_array=sample_size_array,
+                arrays_dict=arrays_dict,
+                title="native matrix with {} noise vs sample sizes".format(noise_type),
+                x_label="sample size")
+
+
+if __name__ == '__main__':
+    count_lags = np.arange(0, 11)
+    read_and_plot_var_cov_hat_native_matrix_by_sample_size_csv(noise_type='gaussian', count_lags=count_lags, fix_number_of_lags=300)
