ff2f3acad7b7558f7168cbe2ec01205011e296e2
Ubuntu <ubuntu@ip-172-31-33-40.us-east-2.compute.internal>
2020-01-11 15:06:26 +0000

    ma3 comments

--- project2/src/diagonal_sample_tvma3.py
+++ project2/src/diagonal_sample_tvma3.py
@@ -6,10 +6,25 @@ import numpy as np
 def diagonal_sample_tvma3(sample_size: int,
                           mean: int,
                           sigma: int,
-                          noise_type: str):
+                          noise_type: str,
+                          noise=None):
+    """
+    Creates the diagonal time-varying MA3 sample, given
+    sample size, mean, sigma and noise type. 
+    :param sample_size: size of a sample to be generated. 
+    :param mean: mean of the noise to generate, if not given. 
+    :param sigma: sigma of the noise to generate, if not given. 
+    :param noise_type: type of the noise to generate, if not given. 
+    :param noise: an array of the size sample_size+3. If it is given (not None), then sample size agreement is checked, and other arguments ignored.
+    The cycle starts from 1, not from 0, because
+    we pass i+1 to theta. 
+    Basically, indexation in Python is x(0), ...x(n-1), 
+    while in formulas, it is for the case x(1), ...x(n).
+    """                              
     noise = create_noise(noise_size=sample_size + 3, mean=mean, sigma=sigma,
                          noise_type=noise_type)
     diagonal_sample_tvma3 = np.full(shape=sample_size, fill_value=np.nan)
+
     for i in range(1, sample_size + 1):
         diagonal_sample_tvma3[i - 1] = noise[i + 2] + \
         coef(t_par=i / sample_size) * noise[i + 1] + \
