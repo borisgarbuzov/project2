89b180b6d8ca4ff99e65882b6f4335cd88f1b2b8
Ubuntu <ubuntu@ip-172-31-33-40.us-east-2.compute.internal>
2020-01-12 12:17:03 +0000

    order

--- project2/src/true_lrv_of_single_t.py
+++ project2/src/true_lrv_of_single_t.py
@@ -2,13 +2,13 @@ from src.coef import coef
 from src.true_cov_of_t import true_cov_scaled_noise_of_t
 
 
+def true_lrv_scaled_noise_of_single_t(sigma, t_par):
+    return true_cov_scaled_noise_of_t(t_par, sigma, lag=0)
+
+
 def true_lrv_ma1_of_single_t(sigma, t_par):
     lrv = sigma ** 2 * (1 + coef(t_par)) ** 2
     return lrv
-
-
-def true_lrv_scaled_noise_of_single_t(sigma, t_par):
-    return true_cov_scaled_noise_of_t(t_par, sigma, lag=0)
 
 
 def true_lrv_ma3_of_single_t():
--- project2/src/true_lrv_of_t.py
+++ project2/src/true_lrv_of_t.py
@@ -3,16 +3,6 @@ from src.true_lrv_of_single_t import true_lrv_ma1_of_single_t, \
 import numpy as np
 
 
-def true_lrv_ma1_of_t(sigma: int, t_par_array: np.array) -> np.array:
-    true_lrv_ma1_array = np.full(shape=len(t_par_array), fill_value=np.nan)
-    for t_index, t_par in enumerate(t_par_array):
-        true_lrv_ma1_array[t_index] = true_lrv_ma1_of_single_t(sigma=sigma,
-                                                               t_par=t_par)
-        print("true_lrv_ma1_of_t t_par left", len(t_par_array) - (t_index + 1))
-
-    return true_lrv_ma1_array
-
-
 def true_lrv_scaled_noise_of_t(sigma: int, t_par_array: np.array) -> np.array:
     true_lrv_scaled_noise_array = np.full(shape=len(t_par_array),
                                           fill_value=np.nan)
@@ -23,6 +13,16 @@ def true_lrv_scaled_noise_of_t(sigma: int, t_par_array: np.array) -> np.array:
               len(t_par_array) - (t_index + 1))
 
     return true_lrv_scaled_noise_array
+    
+    
+def true_lrv_ma1_of_t(sigma: int, t_par_array: np.array) -> np.array:
+    true_lrv_ma1_array = np.full(shape=len(t_par_array), fill_value=np.nan)
+    for t_index, t_par in enumerate(t_par_array):
+        true_lrv_ma1_array[t_index] = true_lrv_ma1_of_single_t(sigma=sigma,
+                                                               t_par=t_par)
+        print("true_lrv_ma1_of_t t_par left", len(t_par_array) - (t_index + 1))
+
+    return true_lrv_ma1_array
 
 
 def true_lrv_ma3_of_t(t_par_array: np.array) -> np.array:
