8e8971854d8595eb0472577e035a6e597068b000
Ubuntu <ubuntu@ip-172-31-33-40.us-east-2.compute.internal>
2020-01-14 12:26:15 +0000

    sigma, tpar

--- project2/src/true_lrv_of_single_t.py
+++ project2/src/true_lrv_of_single_t.py
@@ -1,4 +1,4 @@
-from src.coef import coef
+from src.coef import coef, coef_2, coef_3
 from src.true_cov_of_t import true_cov_scaled_noise_of_t
 
 
@@ -11,5 +11,6 @@ def true_lrv_ma1_of_single_t(sigma, t_par):
     return lrv
 
 
-def true_lrv_ma3_of_single_t():
+def true_lrv_ma3_of_single_t(sigma, t_par):
+    lrv = sigma ** 2 * (1 + coef(t_par) + coef_2(t_par) + coef_3(t_par)) ** 2
     return 0
