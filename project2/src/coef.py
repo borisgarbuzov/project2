8cfd26fecc366569b3d0a68a4da28a8fd6d46230
Ubuntu <ubuntu@ip-172-31-33-40.us-east-2.compute.internal>
2020-01-19 14:29:49 +0000

    changed coefs to deg

--- project2/src/coef.py
+++ project2/src/coef.py
@@ -5,7 +5,7 @@ def coef(t_par):
     :param t_par: a value of t parameter (scaled time) at which we need a coefficient in TVMA process. 
     :return: a value of a coefficient 1 in TVMA at the point of a given t_par. 
     """
-    return 2 + t_par
+    return 2 - t_par
 
 
 def coef_2(t_par):
@@ -25,4 +25,4 @@ def coef_3(t_par):
     :param t_par: a value of t parameter (scaled time) at which we need a third coefficient in TVMA process. 
     :return: a value of a coefficient 3 in TVMA at the point of a given t_par. 
     """
-    return 3 + t_par
+    return -3 + t_par
