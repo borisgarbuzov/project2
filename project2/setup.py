a73d85139215e444c06cf7ffde09fdcde6780c6a
EC2 Default User <ec2-user@ip-172-31-14-206.us-east-2.compute.internal>
2020-03-24 13:32:25 +0000

    add new info in setup file

--- project2/setup.py
+++ project2/setup.py
@@ -4,8 +4,8 @@ setup(
     name='project2_pkg',
     version='0.0.1',
     description='Python package for project 2',
-    author='statistics101',
-    url='https://github.com/statistics101/project2',
+    author='borisgarbuzov',
+    url='https://github.com/borisgarbuzov/project2',
     packages=['src'],
     install_requires=[
         'numpy',
