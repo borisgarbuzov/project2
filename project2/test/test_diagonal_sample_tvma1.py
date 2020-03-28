ab34534b42e8dca97292e1eb6653b6fd83a1077f
EC2 Default User <ec2-user@ip-172-31-14-206.us-east-2.compute.internal>
2020-03-28 13:29:30 +0000

    fix tvar1 test

--- project2/test/test_diagonal_sample_tvma1.py
+++ project2/test/test_diagonal_sample_tvma1.py
@@ -6,30 +6,30 @@ import unittest
 
 class Test_diagonal_sample_tvma1(unittest.TestCase):
     def test_diagonal_sample_tvma1(self, sample_size=8, mean=5, sigma=0.12,
-                                   noise_type= ["bernoulli","gaussian"]):
+                                   noise_type="gaussian"):
 
         print('\n\n===============================================================================')
         print('Testing "diagonal_sample_tvma1"')
         start_time = timer()
         
-        for each in noise_type:
-            returned = diagonal_sample_tvma1(sample_size=sample_size, mean=mean, sigma=sigma, noise_type=each)
-            print('Test parameters:')
-            print('sample_size = ', sample_size)
-            print('mean = ', mean)
-            print('sigma = ', sigma)
-            print('noise_type = ', each)
 
-            print('\nreturned = ', type(returned))
-            if isinstance(returned, list):
-                print('returned shape = ', len(returned))
-            elif isinstance(returned, np.ndarray):
-                print('returned shape = ', returned.shape)
-            print('returned = ', returned)
-                
-            print("\nDuration: {:g} secs".format(timer() - start_time))
-            print('End of test {}'.format('diagonal_sample_tvma1'))
-            print('===============================================================================\n')
+        returned = diagonal_sample_tvma1(sample_size=sample_size, mean=mean, sigma=sigma, noise_type=noise_type)
+        print('Test parameters:')
+        print('sample_size = ', sample_size)
+        print('mean = ', mean)
+        print('sigma = ', sigma)
+        print('noise_type = ', noise_type)
+
+        print('\nreturned = ', type(returned))
+        if isinstance(returned, list):
+            print('returned shape = ', len(returned))
+        elif isinstance(returned, np.ndarray):
+            print('returned shape = ', returned.shape)
+        print('returned = ', returned)
+            
+        print("\nDuration: {:g} secs".format(timer() - start_time))
+        print('End of test {}'.format('diagonal_sample_tvma1'))
+        print('===============================================================================\n')
 
 
 if __name__ == '__main__':
