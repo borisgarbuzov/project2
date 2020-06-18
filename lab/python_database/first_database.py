10f2b19a205b4fe025cbd9cd96a5c65ab7044c0c
EC2 Default User <ec2-user@ip-172-31-28-229.us-east-2.compute.internal>
2020-06-18 20:59:30 +0000

    pg database first commit

---
+++ lab/python_database/first_database.py
@@ -0,0 +1,41 @@
+"""
+install
+python -m pip install psycopg2
+upgrade
+python -m pip install --upgrade pip
+"""
+
+import psycopg2
+
+con = psycopg2.connect(
+  database="boris_db", 
+  user="postgres", 
+  password="Needajob1", 
+  host="database-1-postgres.ctjowhpkuywk.us-east-2.rds.amazonaws.com", 
+  port="5432"
+) 
+ 
+print("Database opened successfully")
+cur = con.cursor()  
+print("dir(cur) = \n", dir(cur))
+myStatusMessage = cur.statusmessage
+print("\n myStatusMessage =", myStatusMessage)
+cur.execute(" select * from customers ")
+  
+  
+rows = cur.fetchall()
+print("rows = \n", rows)
+for row in rows: 
+    print("cust_id = ", row[0])
+    print("cust_name = ", row[1])
+    print("cust_address = ", row[2])
+    print("cust_city = ", row[3])
+    print("cust_state = ", row[4])
+    print("cust_zip = ", row[5])
+    print("cust_country = ", row[6])
+    print("cust_contact = ", row[7])
+    print("cust_email = ", row[8])
+    print("--------------------------")
+
+print("Operation done successfully")  
+con.close() 
